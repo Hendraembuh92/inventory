86bb823f72afddfe84e68ee164634700ba3cec33
Hendraembuh92 <108176493+Hendraembuh92@users.noreply.github.com>
2022-06-26 16:34:42 +0700

    Add files via upload

---
+++ partials/footer.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+	<div class="container my-auto">
+		<div class="copyright text-center my-auto">
+			<span>Copyright © <a href="https://instagram.com/mikailryu_?igshid=YmMyMTA2M2Y=" target="_blank">Muhammad Mikail Rasyid</a></span>
+		</div>
+	</div>
+</footer>
